b132bf25e786ea990207453f35f2d7d7bd32f8de
NauxLiu <nauxliu@gmail.com>
2015-12-19 23:40:54 +0800

    CS fixes

--- src/JWT.php
+++ src/JWT.php
@@ -3,34 +3,34 @@
 namespace Naux;
 
 use Namshi\JOSE\JWS;
+
 class JWT
 {
-	protected $jws = null;
-	protected $secret = null;
-	protected $algo = null;
+    protected $jws = null;
+    protected $secret = null;
+    protected $algo = null;
 
-	public function __construct($secret, $algo = 'HS256'){
-		$this->secret = $secret;
-		$this->algo   = $algo;
-		$this->jws    = new JWS(['typ' => 'JWT', 'alg' => $algo]);
-	}
+    public function __construct($secret, $algo = 'HS256')
+    {
+        $this->secret = $secret;
+        $this->algo   = $algo;
+        $this->jws    = new JWS(['typ' => 'JWT', 'alg' => $algo]);
+    }
 
-	public function decode($token)
-	{
-		$jws = $this->jws->load($token, false);
+    public function decode($token)
+    {
+        $jws = $this->jws->load($token, false);
 
-		if (! $jws->verify($this->secret, $this->algo)) {
+        if (! $jws->verify($this->secret, $this->algo)) {
             throw new Exception('Token Signature could not be verified.');
         }
 
         return (array) $jws->getPayload();
-	}
+    }
 
-	public function encode($payload)
-	{
-		$this->jws->setPayload($payload)->sign($this->secret);
+    public function encode($payload)
+    {
+        $this->jws->setPayload($payload)->sign($this->secret);
         return (string) $this->jws->getTokenString();
-	}
+    }
 }
-
-
